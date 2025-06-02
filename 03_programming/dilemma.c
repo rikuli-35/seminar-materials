ae03d0574ba9a78781aea8f945b64d05a5109095
rikuli-35 <119401921+rikuli-35@users.noreply.github.com>
2025-06-02 15:07:52 +0900

    Update dilemma.c

--- 03_programming/dilemma.c
+++ 03_programming/dilemma.c
@@ -9,8 +9,16 @@ int main() {
     int n;
     int m=rand()%2;
 
+    while(1){
     printf("自白なら0、黙秘なら1を入力してください。:");
-    scanf("%d",&n);
+    if(scanf("%d",&n) != 1) {
+    printf("数字を入力して下さい。\n");
+    while(getchar() != '\n');
+    continue; 
+    } 
+    if(n == 0 || n == 1) break;
+    printf("数字の入力は0か1です。\n");
+    }
 
     if(n==0 && m==0)
     printf("私の懲役は5年、あなたの懲役も5年です。");
