798c37fb5ac0d9ca4221175ca46147b0299d5d30
rikuli-35 <119401921+rikuli-35@users.noreply.github.com>
2026-01-29 18:25:50 +0900

    Update game.h

--- 03_programming/type_card_game/main/game.h
+++ 03_programming/type_card_game/main/game.h
@@ -1,8 +1,35 @@
 #pragma once
 #include <vector>
+#include <string>
 
+// タイプ数・ラウンド数
 constexpr int TYPE_COUNT = 19;
 constexpr int MAX_ROUNDS = 10;
+
+// タイプタグ
+enum class Type {
+
+    NORMAL = 0,
+    FIRE = 1,
+    WATER = 2,
+    ELECTRIC = 3,
+    GRASS = 4,
+    ICE = 5,
+    FIGHTING = 6,
+    POISON = 7,
+    GROUND = 8,
+    FLYING = 9,
+    PSYCHIC = 10,
+    BUG = 11,
+    ROCK = 12,
+    GHOST = 13,
+    DRAGON = 14,
+    DARK = 15,
+    STEEL = 16,
+    FAIRY = 17,
+    UNKNOWN = 18
+
+};
 
 // プレイヤータグ
 enum class PlayerID { 
@@ -22,32 +49,45 @@ enum class GameVersion {
 
 };
 
-// ゲームの状態管理
-enum class GameState {
-
-    COIN_TOSS,
-    ROUND_START,
-    TURN,
-    ROUND_RESULT,
-    GAME_END
-
-};
-
-// プレイヤーデータ
+// プレイヤー情報
 struct Player {
 
     // 手札・山札・捨札・選択中の手札
-    std::vector<int> hand;
-    std::vector<int> deck;
-    std::vector<int> used;
-    std::vector<int> select;
+    std::vector<Type> hand;
+    std::vector<Type> deck;
+    std::vector<Type> used;
+    std::vector<Type> select;
 
     // プレイヤーのスコア
     double score = 0.0;
 
 };
 
-// ゲーム全体データ
+// ゲーム画面タグ
+enum class GameState {
+
+    COIN_TOSS,
+    ROUND_START,
+    DRAW_OR_HANDDEATH,
+    SELECT_PHASE,
+    ROUND_EFFECT,
+    ROUND_RESULT,
+    GAME_END
+
+};
+
+// ゲーム内アクションタグ
+enum class GameCommand {
+
+    NONE,
+    DRAW,
+    HANDDEATH,
+    USE,
+    NEXT_ROUND
+
+};
+
+// ゲーム情報
 struct GameData {
 
     // 2人のプレイヤー
@@ -56,31 +96,44 @@ struct GameData {
     // ラウンド数
     int round = 1;
 
-    // どちらのターンか？
-    PlayerID attacker;
-    PlayerID turn_end;
+    // タイプ数
+    int type_count;
 
-    // 次のラウンドの用意
-    bool round_ready = false;
+    // ターン管理
+    PlayerID current_turn;
 
     // ゲーム状態
-    GameState state = GameState::COIN_TOSS;
+    GameVersion version;
+    GameState state;
+    GameCommand command = GameCommand::NONE;
+
+    // ドローorハンデス選択
+    bool player_prepared = false;
+    bool cpu_prepared = false;
 
 };
 
 // 関数の宣言（準備・初期化）
+std::vector<std::vector<double>> ReadCSV(const std::string& filename);
 void LoadVersion(GameVersion version, GameData& game);
 void InitGame(GameData& game);
 
 // 関数の宣言（ラウンド処理）
 void RoundStart(GameData& game);
-void RoundEnd(GameData& game);
-bool IsRoundFinished(const GameData& game);
+void CoinToss(GameData& game);
+void HandManagement(GameData& game);
+void NextRound(GameData& game);
+void UpdateGame(GameData& game);
 
-// 関数の宣言（プレイヤー操作）
+// 関数の宣言(ラウンド効果)
+void HandRecover(Player& p);
+
+// 関数の宣言（プレイヤー操作）　
 void HandDraw(GameData& game, PlayerID id);
 void HandDeath(GameData& game, PlayerID id);
+void PlayerCommand(GameData& game);
 void PlayerSelect(GameData& game, int index, int count);
 
 // 関数の宣言（CPU操作）
+void CpuCommand(GameData& game);
 void CpuSelect(GameData& game, int count);
